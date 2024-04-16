886b95320ec53237984728c3967ba100a5b0a418
Rosso <25611971+rossop@users.noreply.github.com>
2024-04-16 11:32:19 +0100

    feat: Added feature and sketch extractors
    Added feature and sketch extractors which specialise in extracting
    properties associated with these elements.

--- cad_to_neo4j/extract/__init__.py
+++ cad_to_neo4j/extract/__init__.py
@@ -1,5 +1,5 @@
 """
-Extraction Package
+Extraction Module
 
 This package contains modules and classes for extracting various properties 
 and information from CAD objects. It serves as the extraction part of the 
@@ -9,8 +9,21 @@ storing them in a Neo4j graph database.
 Modules:
     - base_extractor.py: Provides the BaseExtractor class for extracting 
       basic properties such as name, type, and id token from CAD objects.
+    - feature_extractor.py:
+    - sketch_extractor.py:
+    - extractor_factory.py:
+
+Classes:
+    - BaseExtractor: Base class for all extractors.
+    - SketchExtractor: Extractor for Sketch objects.
+    - FeatureExtractor: Extractor for Feature objects.
+    - get_extractor: Factory function to get the appropriate extractor.
 """
 
-from .base_extractor import *
 
-__all__ = base_extractor.__all__
+from .base_extractor import BaseExtractor
+from .sketch_extractor import SketchExtractor
+from .feature_extractor import FeatureExtractor
+from .extractor_factory import get_extractor
+
+__all__ = ['BaseExtractor', 'SketchExtractor', 'FeatureExtractor', 'get_extractor']
