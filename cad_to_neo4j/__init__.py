e4a791470557656ffb3b1b58746c1bce0f6dfa89
Rosso <25611971+rossop@users.noreply.github.com>
2024-04-17 12:05:32 +0100

    docs: Update module docstrings
    Update docstrings for the overall package, extract, and load modules to
    ensure coherence and clarity.
    
    - Update docstring for the overall package __init__.py
    - Update docstring for extract module __init__.py
    - Update docstring for load module __init__.py

--- cad_to_neo4j/__init__.py
+++ cad_to_neo4j/__init__.py
@@ -1,16 +1,17 @@
 """
-cad_to_neo4j Package
+CAD to Neo4j Package
 
-This package provides functionalities to extract, transform, and load CAD model
-data into a Neo4j graph database. It includes utilities for logging, various
-extractors for CAD elements, and a loader for interacting with the Neo4j 
-database.
+This package provides an ELT (Extract, Load, Transform) pipeline for processing CAD models 
+from Autodesk Fusion 360 and storing them in a Neo4j graph database. The pipeline consists 
+of extraction of CAD model data, loading the extracted data into the Neo4j database, and 
+optional transformation of data for further analysis.
 
-Submodules:
-    - utils: Utility functions and classes, including logging setup.
-    - extract: Extractor classes and functions for extracting information from 
-        CAD models.
-    - load: Loader classes and functions for loading data into Neo4j.
+Modules:
+    - extract: Contains modules and classes for extracting various properties and information 
+      from CAD objects.
+    - load: Provides functionalities for loading data into the Neo4j graph database.
+    - transform: (Future implementation) Contains modules and classes for transforming data.
+    - utils: Common utility functions and classes used across the package.
 """
 
 from .utils import *
--- cad_to_neo4j/load/__init__.py
+++ cad_to_neo4j/load/__init__.py
@@ -1,13 +1,13 @@
 """
-Load Package
+Load Module
 
-This package provides functionalities for loading data into the Neo4j graph database.
-It includes modules for establishing connections and inserting nodes and relationships
-into the database.
+This module provides functionalities for loading data into the Neo4j graph database. It includes 
+modules for establishing connections and inserting nodes and relationships into the database, 
+serving as the loading part of the ELT (Extract, Load, Transform) pipeline for processing CAD models.
 
 Modules:
-    - neo4j_loader.py: Functions for interacting with the Neo4j database, including
-      creating nodes and relationships.
+    - neo4j_loader.py: Contains the Neo4jLoader class and functions for interacting with the Neo4j 
+      database, including creating nodes and relationships.
 """
 
 from .neo4j_loader import Neo4jLoader
