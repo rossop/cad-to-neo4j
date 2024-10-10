c549d42530db226454205978378bc97c144d929f
Rosso <25611971+rossop@users.noreply.github.com>
2024-10-11 00:10:37 +0100

    feat: link extrusion feature to taper angle
    Improved the extraction of extrusion features, including support for
    taper angles, and added the ability to link taper angles to their
    corresponding parameters in the graph. This change enhances the model
    parameter relationships and feature detail extraction.
    
    - Add extraction for taper angles in extrusion features
    - Link taper angles to model parameters in the Neo4j database
    - Improve face extraction for extrusion features
    - Refactor extent and face relationship creation

--- cad_to_neo4j/extract/feature/extrude_feature_extractor.py
+++ cad_to_neo4j/extract/feature/extrude_feature_extractor.py
@@ -1,26 +1,32 @@
 """
 Extrude Feature Extractor Module
 
-This module provides an extractor class for extracting information from ExtrudeFeature objects.
+This module provides an extractor class for extracting information from
+ExtrudeFeature objects.
 
 Classes:
     - ExtrudeFeatureExtractor: Extractor for ExtrudeFeature objects.
 """
 from typing import Optional, Any, Dict, List
+
+import traceback
 import adsk.fusion
-import adsk.core, traceback
+import adsk.core
+
 from .feature_extractor import FeatureExtractor
 from ...utils.general_utils import nested_getattr
 
 __all__ = ['ExtrudeFeatureExtractor']
 
+
 class ExtrudeFeatureExtractor(FeatureExtractor):
-    """Extractor for extracting detailed information from ExtrudeFeature objects."""
+    """Extractor for extracting detailed information from ExtrudeFeature
+    objects."""
 
     def __init__(self, obj: adsk.fusion.ExtrudeFeature):
         """Initialize the extractor with the ExtrudeFeature element."""
         super().__init__(obj)
-            
+
     def extract_info(self) -> dict:
         """Extract all information from the ExtrudeFeature element.
 
@@ -29,30 +35,32 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
         """
         feature_info = super().extract_info()
         extrude_info = {
-            'profileTokens': self.profileTokens,
+            'profileTokens': self.profile_tokens,
             'bodies': self.bodies,
-            'startFaces': self.startFaces,
-            'endFaces': self.endFaces,
-            'sideFaces': self.sideFaces,
+            'startFaces': self.start_faces,
+            'endFaces': self.end_faces,
+            'sideFaces': self.side_faces,
             'extent_type': self.extent_type,
             'operation': self.operation,
-            'participantBodies': self.participantBodies,
+            'participantBodies': self.participant_bodies,
+            'taperAngleOne': self.taper_angle_one_token,
+            'taperAngleTwo': self.taper_angle_two_token
         }
         # Add extent one information
-        extentOne_info = self.extentOne
-        if extentOne_info is not None:
-            extrude_info.update(extentOne_info)
-        
+        extent_one_info = self.extent_one
+        if extent_one_info is not None:
+            extrude_info.update(extent_one_info)
+
         # Add extent two information if available
-        extentTwo_info = self.extentTwo
-        if extentTwo_info is not None:
-            extrude_info.update(extentTwo_info)
+        extent_two_info = self.extent_two
+        if extent_two_info is not None:
+            extrude_info.update(extent_two_info)
 
         extrude_info.update(self.start_extent)
         return {**feature_info, **extrude_info}
 
     @property
-    def profileTokens(self):
+    def profile_tokens(self):
         """Extracts the tokens of profiles used by the ExtrudeFeature.
 
         Returns:
@@ -61,7 +69,8 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
         try:
             profiles = self._obj.profile
             if isinstance(profiles, adsk.core.ObjectCollection):
-                return [profile.entityToken for profile in profiles]
+                profile_list = [profile.entityToken for profile in profiles]
+                return profile_list
             return [profiles.entityToken]
         except AttributeError:
             return []
@@ -72,36 +81,52 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
         try:
             return self.extract_ids('bodies', 'entityToken')
         except AttributeError as e:
-            self.logger.error(f'Error extracting bodies: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting bodies: {e}\n'
+                f'{traceback.format_exc()}'
+                )
+            self.logger.error(attribute_error_msg)
             return None
-        
+
     @property
-    def startFaces(self) -> Optional[List[str]]:
+    def start_faces(self) -> Optional[List[str]]:
         """Extracts the IDs of start faces created by the feature."""
         try:
             return self.extract_ids('startFaces', 'entityToken')
         except AttributeError as e:
-            self.logger.error(f'Error extracting start faces: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting start faces: {e}\n'
+                f'{traceback.format_exc()}'
+                )
+            self.logger.error(attribute_error_msg)
             return None
-        
+
     @property
-    def endFaces(self) -> Optional[List[str]]:
+    def end_faces(self) -> Optional[List[str]]:
         """Extracts the IDs of end faces created by the feature."""
         try:
             return self.extract_ids('endFaces', 'entityToken')
         except AttributeError as e:
-            self.logger.error(f'Error extracting end faces: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting end faces: {e}\n'
+                f'{traceback.format_exc()}'
+                )
+            self.logger.error(attribute_error_msg)
             return None
-        
+
     @property
-    def sideFaces(self) -> Optional[List[str]]:
+    def side_faces(self) -> Optional[List[str]]:
         """Extracts the IDs of side faces created by the feature."""
         try:
             return self.extract_ids('sideFaces', 'entityToken')
         except AttributeError as e:
-            self.logger.error(f'Error extracting side faces: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting side faces: {e}\n'
+                f'{traceback.format_exc()}'
+                )
+            self.logger.error(attribute_error_msg)
             return None
-        
+
     @property
     def start_extent(self) -> Dict[str, Any]:
         """Extracts the start extent definition used by the feature."""
@@ -115,49 +140,68 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
         if start_extent_type:
             if isinstance(start_extent_type, adsk.core.Plane):
                 start_extent_info['start_extent_normal'] = [
-                    nested_getattr(self._obj, f'startExtent.profilePlane.normal.x', None),
-                    nested_getattr(self._obj, f'startExtent.profilePlane.normal.y', None),
-                    nested_getattr(self._obj, f'startExtent.profilePlane.normal.z', None)
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.normal.x', None),
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.normal.y', None),
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.normal.z', None)
                 ]
                 start_extent_info['start_extent_origin'] = [
-                    nested_getattr(self._obj, f'startExtent.profilePlane.origin.x', None),
-                    nested_getattr(self._obj, f'startExtent.profilePlane.origin.y', None),
-                    nested_getattr(self._obj, f'startExtent.profilePlane.origin.z', None)
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.origin.x', None),
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.origin.y', None),
+                    nested_getattr(
+                        self._obj, 'startExtent.profilePlane.origin.z', None)
                 ]
-            elif isinstance(start_extent_type, adsk.fusion.ProfilePlaneWithOffsetStartDefinition):
-                start_extent_info['start_extent_offset'] = nested_getattr(self._obj, f'{start_extent_type}.offset', None)
-            elif isinstance(start_extent_type, adsk.fusion.Entity):
-                start_extent_info['start_extent_entity_id'] = nested_getattr(self._obj, f'{start_extent_type}.entityToken', None)
+            elif isinstance(
+                    start_extent_type,
+                    adsk.fusion.ProfilePlaneWithOffsetStartDefinition):
+                start_extent_info['start_extent_offset'] = nested_getattr(
+                    self._obj, f'{start_extent_type}.offset', None)
+            elif isinstance(
+                    start_extent_type,
+                    adsk.fusion.Entity):
+                start_extent_info['start_extent_entity_id'] = nested_getattr(
+                    self._obj, f'{start_extent_type}.entityToken', None)
 
         return start_extent_info
-    
+
     @property
     def extent_type(self) -> Optional[str]:
-        """Extracts the direction of the extrusion.""" # TODO make this consistent
-        OneSideFeatureExtentType = 0
-        TwoSidesFeatureExtentType = 1
-        SymmetricFeatureExtentType = 2
+        """Extracts the direction of the extrusion."""
+        one_side_feature_extent_type: int = 0
+        two_sides_feature_extent_type: int = 1
+        symmetric_feature_extent_type: int = 2
         try:
             direction_value = self._obj.extentType
-            if direction_value == OneSideFeatureExtentType:
+            if direction_value == one_side_feature_extent_type:
                 return 'One Sided'
-            elif direction_value == TwoSidesFeatureExtentType:
+            elif direction_value == two_sides_feature_extent_type:
                 return 'Two Sided'
-            elif direction_value == SymmetricFeatureExtentType:
+            elif direction_value == symmetric_feature_extent_type:
                 return 'Symmetric'
             else:
                 return 'Unknown'
         except AttributeError as e:
-            self.logger.error(f'Error extracting extent one: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting extent one: {e}\n'
+                f'{traceback.format_exc()}'
+                )
+            self.logger.error(attribute_error_msg)
             return None
 
-    def extract_extent_info(self, extent_root: Any, prefix: str) -> Optional[Dict[str, Any]]:
+    def extract_extent_info(self,
+                            extent_root: Any,
+                            prefix: str) -> Optional[Dict[str, Any]]:
         """Extracts the extent definition information.
-        
+
         Args:
             extent_root (Any): The extent definition object.
-            prefix (str): The prefix to add to the keys in the returned dictionary.
-        
+            prefix (str): The prefix to add to the keys in the returned
+                dictionary.
+
         Returns:
             dict: A dictionary containing the extracted extent information.
         """
@@ -165,36 +209,56 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
             if extent_root is not None:
                 extent_info = {
                     f'{prefix}_type': type(extent_root).__name__,
-                    f'{prefix}_taper_angle': nested_getattr(extent_root, 'taperAngle.value', None),
-                    f'{prefix}IsPositiveDirection': getattr(extent_root, 'isPositiveDirection', None),
+                    f'{prefix}_taper_angle': nested_getattr(
+                                                    extent_root,
+                                                    'taperAngle.value',
+                                                    None),
+                    f'{prefix}IsPositiveDirection': getattr(
+                                                        extent_root,
+                                                        'isPositiveDirection',
+                                                        None),
                 }
-                if isinstance(extent_root, adsk.fusion.DistanceExtentDefinition):
-                    extent_info[f'{prefix}_distance'] = nested_getattr(extent_root, 'distance.value', None)
-                elif isinstance(extent_root, adsk.fusion.ThroughAllExtentDefinition):
+                if isinstance(
+                        extent_root,
+                        adsk.fusion.DistanceExtentDefinition):
+                    extent_info[f'{prefix}_distance'] = \
+                        nested_getattr(extent_root, 'distance.value', None)
+                elif isinstance(
+                        extent_root,
+                        adsk.fusion.ThroughAllExtentDefinition):
                     extent_info[f'{prefix}_distance'] = 'Through All'
-                elif isinstance(extent_root, adsk.fusion.ToEntityExtentDefinition):
-                    extent_info[f'{prefix}_offset'] = nested_getattr(extent_root, 'distance.value', None)
-                    extent_info[f'{prefix}_object_id'] = nested_getattr(extent_root, 'entity.entityToken', None)
+                elif isinstance(
+                        extent_root,
+                        adsk.fusion.ToEntityExtentDefinition):
+                    extent_info[f'{prefix}_offset'] = \
+                        nested_getattr(extent_root, 'distance.value', None)
+                    extent_info[f'{prefix}_object_id'] = \
+                        nested_getattr(extent_root, 'entity.entityToken', None)
 
                 return extent_info
 
             return None
+
         except AttributeError as e:
-            self.logger.error(f'Error extracting extent info: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting extent info: {e}\n'
+                f'{traceback.format_exc()}'
+            )
+            self.logger.error(attribute_error_msg)
             return None
 
     @property
-    def extentOne(self) -> Optional[Dict[str, Any]]:
+    def extent_one(self) -> Optional[Dict[str, Any]]:
         """Extracts the extent one definition used by the feature."""
-        extentOne_root = getattr(self._obj, 'extentOne', None)
-        return self.extract_extent_info(extentOne_root, 'extentOne')
+        extent_one_root = getattr(self._obj, 'extentOne', None)
+        return self.extract_extent_info(extent_one_root, 'extentOne')
 
     @property
-    def extentTwo(self) -> Optional[Dict[str, Any]]:
+    def extent_two(self) -> Optional[Dict[str, Any]]:
         """Extracts the extent two definition used by the feature."""
-        extentTwo_root = getattr(self._obj, 'extentTwo', None)
-        return self.extract_extent_info(extentTwo_root, 'extentTwo')
-    
+        extent_two_root = getattr(self._obj, 'extentTwo', None)
+        return self.extract_extent_info(extent_two_root, 'extentTwo')
+
     @property
     def operation(self) -> Optional[str]:
         """Extracts the type of operation performed by the extrusion."""
@@ -210,18 +274,55 @@ class ExtrudeFeatureExtractor(FeatureExtractor):
             operation_val = getattr(self._obj, 'operation', None)
             return operation_mapping[operation_val] if operation_val else None
         except AttributeError as e:
-            self.logger.error(f'Error extracting operation: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting operation: {e}\n'
+                f'{traceback.format_exc()}'
+            )
+            self.logger.error(attribute_error_msg)
             return None
 
     @property
-    def participantBodies(self) -> Optional[List[str]]:
-        """Extracts the list of bodies that will participate in the feature when the operation is a cut or intersection."""
+    def participant_bodies(self) -> Optional[List[str]]:
+        """Extracts the list of bodies that will participate in the feature
+        when the operation is a cut or intersection."""
         try:
             self.roll_timeline_to_before_feature()
-            participantBodies = getattr(self._obj, 'participantBodies', [])
-            ids = [body.entityToken for body in participantBodies]
+            participant_bodies = getattr(self._obj, 'participantBodies', [])
+            ids = [body.entityToken for body in participant_bodies]
             self.roll_timeline_to_after_feature()
             return ids
         except AttributeError as e:
-            self.logger.error(f'Error extracting participant bodies: {e}\n{traceback.format_exc()}')
+            attribute_error_msg: str = (
+                f'Error extracting participant bodies: {e}\n'
+                f'{traceback.format_exc()}'
+            )
+            self.logger.error(attribute_error_msg)
+            return None
+
+    @property
+    def taper_angle_one_token(self) -> Optional[str]:
+        """
+        Extracts the entity token for the parameter controlling the taper angle
+        for side one of the extrusion.
+        """
+        try:
+            taper_angle_one = self._obj.taperAngleOne
+            if taper_angle_one:
+                return taper_angle_one.entityToken
+            return None
+        except AttributeError:
+            return None
+
+    @property
+    def taper_angle_two_token(self) -> Optional[str]:
+        """
+        Extracts the entity token for the parameter controlling the taper angle
+        for side two of the extrusion.
+        """
+        try:
+            taper_angle_two = self._obj.taperAngleTwo
+            if taper_angle_two:
+                return taper_angle_two.entityToken
+            return None
+        except AttributeError:
             return None
--- cad_to_neo4j/transform/core/strategies/feature_transformer.py
+++ cad_to_neo4j/transform/core/strategies/feature_transformer.py
@@ -10,6 +10,7 @@ Classes:
 import traceback
 from ..base_transformer import BaseTransformer
 
+
 class FeatureTransformer(BaseTransformer):
     """
     FeatureTransformer
@@ -17,9 +18,12 @@ class FeatureTransformer(BaseTransformer):
     A class to handle feature-based transformations.
 
     Methods:
-        transform(execute_query): Runs all feature-related transformation methods.
-        create_profile_relationships(execute_query): Creates 'USES_PROFILE' relationships between features and profiles.
-        link_feature_to_extents_and_faces(execute_query): Links features to their extents and faces based on various properties.
+        transform(execute_query): Runs all feature-related transformation
+            methods.
+        create_profile_relationships(execute_query): Creates 'USES_PROFILE'
+            relationships between features and profiles.
+        link_feature_to_extents_and_faces(execute_query): Links features to
+            their extents and faces based on various properties.
     """
     def transform(self, execute_query):
         """
@@ -32,13 +36,19 @@ class FeatureTransformer(BaseTransformer):
             dict: The result values from the query execution.
         """
         results = {}
-        results['create_profile_relationships'] = self.create_profile_relationships(execute_query)
-        results['create_feature_to_extents_and_faces_relationships'] = self.create_feature_to_extents_and_faces_relationships(execute_query)
+        results['create_profile_relationships'] = \
+            self.create_profile_relationships(execute_query)
+        results['create_feature_to_extents_and_faces_relationships'] = \
+            self.create_feature_to_extents_and_faces_relationships(
+                execute_query)
+        results['link_taper_angles_to_parameters'] = \
+            self.link_taper_angles_to_parameters(execute_query)
         return results
-    
+
     def create_profile_relationships(self, execute_query):
         """
-        Creates 'USES_PROFILE' relationships between feature and profiles based on the profileTokens list.
+        Creates 'USES_PROFILE' relationships between feature and profiles based
+        on the profileTokens list.
 
         Args:
             execute_query (function): Function to execute a Cypher query.
@@ -48,12 +58,15 @@ class FeatureTransformer(BaseTransformer):
         """
         queries = [
             """
-            MATCH (f:`Feature`)
+            MATCH (f:Feature)
             WHERE f.profileTokens IS NOT NULL
             UNWIND f.profileTokens AS profile_token
-            MATCH (p:`Profile` {entityToken: profile_token})
+            MATCH (p:Profile)
+            WHERE p.entityToken = profile_token
             MERGE (f)-[:USES_PROFILE]->(p)
-            RETURN f.entityToken AS feature_id, collect(p.entityToken) AS profile_ids
+            RETURN
+                f.entityToken AS feature_id,
+                collect(p.entityToken) AS profile_ids
             """,
             ]
         results = []
@@ -62,7 +75,11 @@ class FeatureTransformer(BaseTransformer):
             try:
                 results.extend(execute_query(query))
             except Exception as e:
-                self.logger.error(f'Exception executing query: {query}\n{e}\n{traceback.format_exc()}')
+                exception_msg: str = (
+                    f'Exception executing query: {query}\n'
+                    f'{e}\n{traceback.format_exc()}'
+                    )
+                self.logger.error(exception_msg)
         return results
 
     def create_feature_to_extents_and_faces_relationships(self, execute_query):
@@ -81,31 +98,36 @@ class FeatureTransformer(BaseTransformer):
             WHERE f.extentOne IS NOT NULL OR f.extentTwo IS NOT NULL
             OPTIONAL MATCH (e1 {entityToken: f.extentOne_object_id})
             OPTIONAL MATCH (e2 {entityToken: f.extentTwo_object_id})
-            
-            FOREACH (ignore IN CASE WHEN f.extentOne_object_id IS NOT NULL THEN [1] ELSE [] END |
+
+            FOREACH (ignore IN CASE WHEN f.extentOne_object_id IS NOT NULL
+                THEN [1] ELSE [] END |
                 MERGE (f)-[:HAS_extentOne]->(e1)
             )
-            FOREACH (ignore IN CASE WHEN f.extentTwo_object_id IS NOT NULL THEN [1] ELSE [] END |
+            FOREACH (ignore IN CASE WHEN f.extentTwo_object_id IS NOT NULL
+                THEN [1] ELSE [] END |
                 MERGE (f)-[:HAS_extentTwo]->(e2)
             )
-            
+
             WITH f
             OPTIONAL MATCH (sf {entityToken: f.startFaces})
             OPTIONAL MATCH (ef {entityToken: f.endFaces})
             OPTIONAL MATCH (sif {entityToken: f.sideFaces})
-            
-            FOREACH (ignore IN CASE WHEN f.startFaces IS NOT NULL THEN [1] ELSE [] END |
+
+            FOREACH (ignore IN CASE WHEN f.startFaces IS NOT NULL
+                THEN [1] ELSE [] END |
                 MERGE (f)-[:HAS_START_FACE]->(sf)
             )
-            FOREACH (ignore IN CASE WHEN f.endFaces IS NOT NULL THEN [1] ELSE [] END |
+            FOREACH (ignore IN CASE WHEN f.endFaces IS NOT NULL
+                THEN [1] ELSE [] END |
                 MERGE (f)-[:HAS_END_FACE]->(ef)
             )
-            FOREACH (ignore IN CASE WHEN f.sideFaces IS NOT NULL THEN [1] ELSE [] END |
+            FOREACH (ignore IN CASE WHEN f.sideFaces IS NOT NULL
+                THEN [1] ELSE [] END |
                 MERGE (f)-[:HAS_SIDE_FACE]->(sif)
             )
-            
-            RETURN f.entityToken AS feature_id, 
-                f.extentOne_object_id AS extentOne_id, 
+
+            RETURN f.entityToken AS feature_id,
+                f.extentOne_object_id AS extentOne_id,
                 f.extentTwo_object_id AS extentTwo_id,
                 f.startFaces AS startFaces,
                 f.endFaces AS endFaces,
@@ -113,10 +135,53 @@ class FeatureTransformer(BaseTransformer):
             """,
         ]
         results = []
-        self.logger.info('Creating relationships between features, extents and faces')
+        tranformation_msg: str = \
+            'Creating relationships between features, extents and faces'
+        self.logger.info(tranformation_msg)
         for query in queries:
             try:
                 results.extend(execute_query(query))
             except Exception as e:
-                self.logger.error(f'Exception executing query: {query}\n{e}\n{traceback.format_exc()}')
+                general_exception_msg: str = (
+                    f'Exception executing query: {query}\n'
+                    f'{e}\n{traceback.format_exc()}'
+                    )
+                self.logger.error(general_exception_msg)
+        return results
+
+    def link_taper_angles_to_parameters(self, execute_query):
+        """
+        Links the taper angles of extrude features to their corresponding model
+        parameters.
+
+        Args:
+            execute_query (function): Function to execute a Cypher query.
+        """
+        queries = [
+            """
+            MATCH (f:Feature)
+            WHERE f.taperAngleOne IS NOT NULL
+            MATCH (p:ModelParameter {entityToken: f.taperAngleOne})
+            MERGE (f)-[:USES_PARAMETER]->(p)
+            RETURN f.entityToken AS feature_id, p.entityToken AS parameter_id
+            """,
+            """
+            MATCH (f:Feature)
+            WHERE f.taperAngleTwo IS NOT NULL
+            MATCH (p:ModelParameter {entityToken: f.taperAngleTwo})
+            MERGE (f)-[:USES_PARAMETER]->(p)
+            RETURN f.entityToken AS feature_id, p.entityToken AS parameter_id
+            """
+        ]
+        results = []
+        self.logger.info('Linking taper angles to parameters')
+        for query in queries:
+            try:
+                results.extend(execute_query(query))
+            except Exception as e:
+                general_Exception_msg: str = (
+                    f'Exception executing query: {query}\n'
+                    f'{e}\n{traceback.format_exc()}'
+                )
+                self.logger.error(general_Exception_msg)
         return results
